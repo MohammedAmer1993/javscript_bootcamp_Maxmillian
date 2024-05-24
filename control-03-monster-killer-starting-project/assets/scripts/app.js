506dda6e5b9f884721ab92fe809d8bc0b02df6c1
Mohammed Amer <loard.amer.2010@gmail.com>
2024-05-25 00:06:32 +0300

    clean up some typos

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -2,11 +2,17 @@ const MAX_NORMAL_PLAYER_ATTACK = 10;
 const MAX_MONSTER_ATTACK = 12;
 const MAX_STRONG_PALYER_ATTACK = 14;
 const HEAL_VALUE = 6;
+const LOG_EVENT_ATTACK = "PLAYER ATTACK";
+const LOG_EVENT_STRONG = "PLAYER STRONG ATTACK";
+const LOG_EVENT_HEAL = "PLAYER HEAL";
+const LOG_EVENT_MONSTER = "MONSTER ATTACK";
+const LOG_EVENT_ENDGAME = "FINAL RESUTL";
 
 let MaxLife;
 let playerCurrentLife;
 let monsterCurrentLife;
 let hasBounesLife = true;
+let logArray = [];
 
 const initiallife = prompt("what is the life value", "100");
 MaxLife = parseInt(initiallife);
@@ -21,6 +27,7 @@ adjustHealthBars(MaxLife);
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healHandler);
+logBtn.addEventListener("click", logHandler);
 
 function attackHandler() {
   hitMonster(MAX_NORMAL_PLAYER_ATTACK);
@@ -31,22 +38,45 @@ function strongAttackHandler() {
 }
 
 function healHandler() {
-  increasePlayerHealth(HEAL_VALUE);
+  let healvalue;
   if (playerCurrentLife + HEAL_VALUE >= MaxLife) {
-    playerCurrentLife = MaxLife;
+    healvalue = MaxLife - playerCurrentLife;
   } else {
-    playerCurrentLife += HEAL_VALUE;
+    healvalue = HEAL_VALUE;
   }
+  increasePlayerHealth(healvalue);
+  playerCurrentLife += healvalue;
+  addToLog(LOG_EVENT_HEAL, healvalue, playerCurrentLife, monsterCurrentLife);
   endRound();
+}
+
+function logHandler() {
+  console.log(logArray);
 }
 
 function hitMonster(attackValue) {
   const monsterHit = dealMonsterDamage(attackValue);
   monsterCurrentLife -= monsterHit;
+  if ((attackValue = MAX_NORMAL_PLAYER_ATTACK)) {
+    addToLog(
+      LOG_EVENT_ATTACK,
+      monsterHit,
+      playerCurrentLife,
+      monsterCurrentLife
+    );
+  } else {
+    addToLog(
+      LOG_EVENT_STRONG,
+      monsterHit,
+      playerCurrentLife,
+      monsterCurrentLife
+    );
+  }
   endRound();
 }
 
 function endRound() {
+  let matchState;
   let healthBeforeDeath = playerCurrentLife;
   const fireBack = dealPlayerDamage(MAX_MONSTER_ATTACK);
   playerCurrentLife -= fireBack;
@@ -57,12 +87,34 @@ function endRound() {
     removeBonusLife();
     setPlayerHealth(healthBeforeDeath);
   }
+  addToLog(LOG_EVENT_MONSTER, fireBack, playerCurrentLife, monsterCurrentLife);
   if (playerCurrentLife <= 0 && monsterCurrentLife > 0) {
     alert("LOSER HAHAHAHA");
+    matchState = "LOSER";
+    addToLog(
+      LOG_EVENT_ENDGAME,
+      matchState,
+      playerCurrentLife,
+      monsterCurrentLife
+    );
   } else if (monsterCurrentLife <= 0 && playerCurrentLife > 0) {
     alert("WAY TO GO CHAMP");
+    matchState = "WINNER";
+    addToLog(
+      LOG_EVENT_ENDGAME,
+      matchState,
+      playerCurrentLife,
+      monsterCurrentLife
+    );
   } else if (monsterCurrentLife <= 0 && playerCurrentLife <= 0) {
     alert("Draw");
+    matchState = "DRAW";
+    addToLog(
+      LOG_EVENT_ENDGAME,
+      matchState,
+      playerCurrentLife,
+      monsterCurrentLife
+    );
   }
   if (playerCurrentLife <= 0 || monsterCurrentLife <= 0) {
     reset();
@@ -73,4 +125,29 @@ function reset() {
   resetGame(MaxLife);
   playerCurrentLife = MaxLife;
   monsterCurrentLife = MaxLife;
+}
+
+function addToLog(event, value, playerHealth, monsterHealth) {
+  if (
+    event !== LOG_EVENT_ATTACK &&
+    event !== LOG_EVENT_STRONG &&
+    event !== LOG_EVENT_MONSTER &&
+    event !== LOG_EVENT_HEAL &&
+    event !== LOG_EVENT_ENDGAME
+  ) {
+    return;
+  }
+  let logEntry = {
+    event: event,
+    value: value,
+    playerHealth: playerHealth,
+    monsterHealth: monsterHealth,
+  };
+  if (event === LOG_EVENT_ATTACK || event === LOG_EVENT_STRONG) {
+    logEntry.target = "MONSTER";
+  }
+  if (event === LOG_EVENT_MONSTER) {
+    logEntry.target = "PLAYER";
+  }
+  logArray.push(logEntry);
 }
