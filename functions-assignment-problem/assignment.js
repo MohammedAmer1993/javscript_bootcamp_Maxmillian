59017b15cb08aeb185e5a4f81ca4eb2f97ee9fa6
Mohammed Amer <loard.amer.2010@gmail.com>
2024-05-29 04:24:45 +0300

    assignment function done

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,34 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+const sayHello = (name) => {
+  console.log("Hi " + name);
+};
+
+const sayHello2 = (greeting, name) => {
+  console.log(greeting + " " + name);
+};
+
+const sayHello3 = (name) => "hi " + name;
+
+const sayHello4 = () => console.log("hi mohammed");
+
+const sayHello5 = (name, greeting = "Hi ") => {
+  console.log(greeting + name);
+};
+
+function checkinput(cb, ...strings) {
+  let isEmpty = false;
+  for (el of strings) {
+    if (!el) {
+      isEmpty = true;
+      break;
+    }
+  }
+  if (!isEmpty) {
+    cb();
+  }
 }
 
-sayHello();
+function checkArg() {
+  console.log("all input is valid string");
+}
+sayHello("Max");
+checkinput(checkArg, "mohammed", "amer", "is", "here");
