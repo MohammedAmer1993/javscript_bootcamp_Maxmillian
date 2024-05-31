cd05d7e0e29f88033ac6c171ba8192820c082c05
Mohammed Amer <loard.amer.2010@gmail.com>
2024-05-31 19:17:50 +0300

    added dom-06 and clean up for working

--- working/app.js
+++ working/app.js
@@ -1,10 +1,21 @@
-const btn = document.getElementById("button");
-const inp = document.querySelector("input");
+const button = document.querySelector("button");
 const li1 = document.querySelector("#frist");
+const li2 = document.querySelector("#second");
+const li3 = document.querySelector("#third");
+const ul = document.querySelector("ul");
+const div = document.querySelector("div");
 
-btn.addEventListener("click", headerHandler);
+let x = 0;
+const itm = [li1, li2, li3];
+const opt = ["opt1", "opt2", "opt3"];
 
-function headerHandler() {
-  document.body.children.main.textContent = "finally worked";
-  li1.className = "opt1";
+let caller = clickHandler.bind(this, itm, opt);
+
+button.addEventListener("click", caller);
+
+function clickHandler(itemArr, optArr) {
+  itemArr[x % 3].className = optArr[0];
+  itemArr[(x + 1) % 3].className = optArr[1];
+  itemArr[(x + 2) % 3].className = optArr[2];
+  ++x;
 }
