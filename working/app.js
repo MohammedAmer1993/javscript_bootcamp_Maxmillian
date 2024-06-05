9fde6aefb3af56835ded0b79d6c22b266e727c32
Mohammed Amer <loard.amer.2010@gmail.com>
2024-06-05 03:05:20 +0300

    clean up after testing maps and foreEach

--- working/app.js
+++ working/app.js
@@ -19,3 +19,66 @@ function clickHandler(itemArr, optArr) {
   itemArr[(x + 2) % 3].className = optArr[2];
   ++x;
 }
+
+// const people = [
+//   { id: 6, name: "Thomas H.Carson", born: 1956 },
+//   { id: 4, name: "Alan Turning", born: 1912 },
+//   { id: 1, name: "Donald Kuth", born: 1938 },
+//   { id: 7, name: "Peter Norvig", born: 1956 },
+//   { id: 10, name: "Randal Braynt", born: 1952 },
+//   { id: 2, name: "Vin Cerf", born: 1943 },
+//   { id: 8, name: "Jon Bently", born: 1953 },
+//   { id: 9, name: "Grady Booch", born: 1955 },
+//   { id: 5, name: "John Von Neuman", born: 1903 },
+//   { id: 3, name: "Hal Aspelson", born: 1947 },
+// ];
+
+// let m = people.findIndex((el, idx, prs) => {
+//   return el.name === "Jon Bently";
+// });
+
+// function reducer(previous, current) {
+//   previous[current.id] = current;
+//   return previous;
+// }
+
+// const wholeArrIntoObj = people.reduce(reducer, {});
+// console.log(wholeArrIntoObj);
+
+// const arr = [
+//   [3, 2, 5, 1],
+//   [6, 2, 4, 8, 3, 4],
+//   [2, 5],
+//   [3, 5, 2, 5],
+//   [3],
+//   [3, 6, 7, 5, 3, 5],
+// ];
+
+// let intial = {
+//   shortest: null,
+//   longest: [],
+// };
+
+// function reducer(previous, current) {
+//   shortValue = previous.shortest ? previous.shortest.length : Number.MAX_VALUE;
+//   return {
+//     shortest: shortValue < current.length ? previous.shortest : current,
+//     longest:
+//       previous.longest.length > current.length ? previous.longest : current,
+//   };
+// }
+
+// let y = arr.reduce(reducer, intial);
+
+// console.log(y);
+
+// console.log(arr[2].includes(5));
+
+let arr = [3, 5, 6, 7];
+let newarr = [];
+
+arr.forEach((ele, idx, numbers) => {
+  newarr.push({ idx: idx, value: ele });
+});
+
+console.log(newarr);
